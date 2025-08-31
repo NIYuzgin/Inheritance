48ea8083354ce21c45ea9314dcdf21f9decb1f32
Николай Немов <prost91@outlook.com>
2025-08-31 17:58:05 +0300

    Load DONE

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -79,16 +79,21 @@ public:
 		os << age;
 		return os;
 	}
-
+	virtual std::istream& scan(std::istream& is) {
+		return is >> last_name >> first_name >> age;
+	}
 };
 
 int Human::count = 0;	//Инициализация статической переменной (относится к определению класса - Class definition)
 
-
-
 std::ostream& operator<<(std::ostream& os, const Human& obj) {
 	return obj.info(os);
 }
+
+std::istream& operator>>(std::istream& is, Human& obj) {
+	return obj.scan(is);
+}
+
 
 #define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
@@ -156,6 +161,23 @@ public:
 		os << attendance;
 		return os;
 	}
+
+	std::istream& scan(std::istream& is)override {
+		Human::scan(is);
+		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
+		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество символов из файла,
+		// и сохраняет их в NTL (NULL - Terminated Line)
+
+		// Удаляем лишние пробелы в конце прочитанной строки
+		for (int i = SPECIALITY_WIDTH-1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
+
+		// Удаляем лишние пробелы в начале прочитанной строки
+		while (sz_buffer[0] == ' ')
+			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
+		speciality = sz_buffer; // Сохраняем специальность в соответствующее поле
+		is >> group >> rating >> attendance;
+		return is;
+	}
 };
 
 #define TEACHER_TAKES_PARAMETERS const std::string& speciality, int experiance
@@ -183,7 +205,6 @@ public:
 
 	//			Constructors:
 
-
 	Teacher(HUMAN_TAKES_PARAMETERS, TEACHER_TAKES_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS) {
 		set_speciality(speciality);
 		set_experience(experiance);
@@ -201,6 +222,23 @@ public:
 		os.width(EXPERIANCE_WIDTH);
 		os << experiance;
 		return os;
+	}
+
+	std::istream& scan(std::istream& is)override {
+		Human::scan(is);
+		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
+		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество символов из файла,
+		// и сохраняет их в NTL (NULL - Terminated Line)
+
+		// Удаляем лишние пробелы в конце прочитанной строки
+		for (int i = SPECIALITY_WIDTH-1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
+
+		// Удаляем лишние пробелы в начале прочитанной строки
+		while (sz_buffer[0] == ' ')
+			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
+		speciality = sz_buffer; // Сохраняем специальность в соответствующее поле
+		is >> experiance;
+		return is;
 	}
 };
 
@@ -231,6 +269,12 @@ public:
 	std::ostream& info(std::ostream& os)const override {
 		return Student::info(os) << " " << get_subject();
 	}
+
+	std::istream& scan(std::istream& is)override {
+		Student::scan(is);
+		std::getline(is, subject);
+		return is;
+	}
 };
 
 void Print(Human* group[], const int n) {
@@ -253,6 +297,14 @@ void Save(Human* group[], const int n, const std::string& filename) {
 	std::string cmd = "notepad ";
 	cmd += filename;
 	system(cmd.c_str());	//Метод возвращает строку в виде массива символов (char*);
+}
+Human* HumanFactory(const std::string& type) {
+	Human* human = nullptr;
+	if (strstr(type.c_str(), "Human"))human = new Human("", "", 0);
+	else if (strstr(type.c_str(), "Student"))human = new Student("", "", 0, "", "", 0, 0);
+	else if (strstr(type.c_str(), "Graduate"))human = new Graduate("", "", 0, "", "", 0, 0,"");
+	else if (strstr(type.c_str(), "Teacher"))human = new Teacher("", "", 0, "", 0);
+	return human;
 }
 
 Human** Load(const std::string& filename, int& n) {
@@ -282,7 +334,16 @@ Human** Load(const std::string& filename, int& n) {
 	fin.seekg(0);	// Метод seekg(n) переводит Get - курсор (на чтение) в указанную позицию 'n';
 	
 	cout << "Position " << fin.tellg() << endl;
-	 
+
+	// 4) Загружаем объекты из файла:
+	for (int i = 0; !fin.eof(); ) {
+		std::string buffer;
+		std::getline(fin, buffer, ':');
+		if (buffer.size() < 5)continue;
+		group[i] = HumanFactory(buffer);
+		fin >> *group[i];
+		i++;
+	}
 	// ?) Закрываем файл:
 	fin.close();
 	return group;
